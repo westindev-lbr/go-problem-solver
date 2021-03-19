b30351aca0295da4011553da1c68e0b385608e42
Axel Labarre <axel.labarre@etud.univ-paris8.fr>
2021-03-19 16:51:42 +0100

    Ajout fichier gitignore + struc pierre main.c

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
+struct pierre_noire{
+    int x;
+    int y;
+};
 
+struct pierre_blanche{
+    int x;
+    int y;
+};
+
+typedef struct pierre_noire noire;
+typedef struct pierre_blanche blanche;
+
+
+/*Pierre est isolé quand 
+aucune autre pierre n'est a coté. 
+x',y' ,  +1 ou -1 = vide   
+
+*/
+/*
+int est_isole(noire n){
+    if (p.x)
+    switch
+
+}
+*/
 
 
 int main (){
+    noire n;
+    n.x = 0;
+    n.y = 0; 
+    printf("pierre noire = (%d,%d)\n", n.x,n.y);
+
+
+
     return 0;
 }
